f2795839aeae5cfa6165094d904af4be305b40a5
JTorres1026 <jtorresiba26@gmail.com>
2025-05-16 09:33:40 -0600

    feat: Card and brand section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,26 +20,89 @@
 
 
 <section>
-    <div class="bg-blue-700 w-full h-140 bg-cover bg-center"
+    <div class="w-full h-140 bg-cover bg-center pt-20 "
         style="background-image: url('images/Recursos/banner-image.png');"
     >
 
     <!--Contenedor del texto del banner-->
     <div class="flex flex-col ">
 
-        <div class="flex flex-row w-full">
+        <div class="flex flex-row w-full pl-5">
             <p class="text-white font-bold text-4xl">DURABILIDAD</p>
         </div>
 
-        <div class="flex flex-row w-full">
-            <p class="text-white font-bold text-4xl">POTENCIA Y COMODIDAD</p>
+        <div class="flex flex-row w-full pl-5">
+            <p class="text-white font-bold text-4xl ">POTENCIA Y COMODIDAD</p>
         </div>
 
-        <div class="flex flex-row w-full">
-            <a href="/catalog" class="text-white text-2xl">¡Adquiérela ahora! </a>
+        <div class="flex flex-row w-full pl-5 pt-15">
+            <a href="/catalog" class="text-white text-2xl border p-3 rounded-4xl ">¡Adquiérela ahora! </a>
         </div>
 
     </div>
 
     </div>
+</section>
+
+
+<section>
+    <div class="flex flex-col bg-primary">
+        <div class="flex flex-row w-full justify-center">
+            <p class="text-black font-bold text-center text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">Nuestras marcas</p>
+        </div>
+
+        <div class="flex flex-row w-full justify-around mt-5">
+
+            <div class="h-40 w-40"><img src="images/Navbar/LOGOGLOBAL.png" alt="Suzuki"></div>
+            <div class="h-40 w-40"><img src="images/Navbar/LOGOGLOBAL.png" alt="Bajaj"></div>
+            <div class="h-40 w-40"><img src="images/Navbar/LOGOGLOBAL.png" alt="TVS"></div>
+            <div class="h-40 w-40"><img src="images/Navbar/LOGOGLOBAL.png" alt="Freedom"></div>
+            <div class="h-40 w-40"><img src="images/Navbar/LOGOGLOBAL.png" alt="Treck"></div>
+
+
+        </div>
+
+    </div>
+
+</section>
+
+
+<section>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 p-4">
+
+        <!-- Card 1 -->
+        <div class="flex flex-col bg-white shadow-md rounded-lg p-3">
+          <p class="text-lg font-bold">HONDA</p>
+
+          <div class="flex flex-row justify-center mt-2">
+            <div class="bg-amber-400 h-40 w-50"><img src="images/Recursos/red-moto.png" alt="Motocicleta Honda"></div>
+          </div>
+
+          <div class="flex flex-row justify-end mt-3">
+          <p class="text-gray-600">XR 190 L</p>
+         </div>
+
+         <div class="flex flex-row justify-end mt-3">
+            <p class="text-gray-600">Q 13,000.00</p>
+           </div>
+
+        </div>
+
+
+
+        <!-- Card 2 -->
+        <div class="bg-white shadow-md rounded-lg p-6">
+          <h2 class="text-lg font-semibold">Card 2</h2>
+          <p class="text-gray-600">Contenido de la tarjeta 2.</p>
+        </div>
+
+        <!-- Card 3 -->
+        <div class="bg-white shadow-md rounded-lg p-6">
+          <h2 class="text-lg font-semibold">Card 3</h2>
+          <p class="text-gray-600">Contenido de la tarjeta 3.</p>
+        </div>
+
+        <!-- Agrega más tarjetas si lo deseas -->
+      </div>
+
 </section>
